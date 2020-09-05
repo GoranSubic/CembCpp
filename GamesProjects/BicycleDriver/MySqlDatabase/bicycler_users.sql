4be294474c65dfa399191e18e37853ac477eb6d4
Goran Subic <gsubic@gmail.com>
2020-09-05 11:55:03 +0200

    Implemented ORM - SOCI - solution files

--- GamesProjects/BicycleDriver/MySqlDatabase/bicycler_users.sql
+++ GamesProjects/BicycleDriver/MySqlDatabase/bicycler_users.sql
@@ -27,7 +27,7 @@ CREATE TABLE `users` (
   `nickname` varchar(10) NOT NULL,
   `score` int DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,7 +36,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` (`id`, `nickname`, `score`) VALUES (1,'No Name',21),(2,' Goran',11),(3,' Aca',72),(4,'No Name1',63),(5,'No Name2',26),(6,' Igor',37);
+INSERT INTO `users` (`id`, `nickname`, `score`) VALUES (1,'No Name',24),(2,' Goran',17),(3,' Aca',72),(4,'No Name1',63),(5,'No Name2',26),(6,' Igor',37),(7,' Milos',1);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -49,4 +49,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-08-28 10:40:59
+-- Dump completed on 2020-09-05 11:52:00
